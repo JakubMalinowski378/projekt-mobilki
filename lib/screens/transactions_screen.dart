a0749d9d0fe52911784052bab571d7b35c0d9d43
Jakub Malinowski <jakubmalinowski378@gmail.com>
2026-01-07 10:38:04 +0100

    Refactor transactions screen layout and add currency conversion display

--- lib/screens/transactions_screen.dart
+++ lib/screens/transactions_screen.dart
@@ -17,7 +17,7 @@ class TransactionsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context)!;
-    
+
     return Scaffold(
       appBar: AppBar(
         title: Text(l10n.transactions),
@@ -34,102 +34,183 @@ class TransactionsScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Consumer4<TransactionProvider, CategoryProvider, CurrencyProvider, SettingsProvider>(
-        builder: (context, transactionProvider, categoryProvider, currencyProvider, settingsProvider, _) {
-          if (transactionProvider.isLoading) {
-            return const Center(child: CircularProgressIndicator());
-          }
+      body:
+          Consumer4<
+            TransactionProvider,
+            CategoryProvider,
+            CurrencyProvider,
+            SettingsProvider
+          >(
+            builder:
+                (
+                  context,
+                  transactionProvider,
+                  categoryProvider,
+                  currencyProvider,
+                  settingsProvider,
+                  _,
+                ) {
+                  if (transactionProvider.isLoading) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
 
-          final transactions = transactionProvider.transactions;
+                  final transactions = transactionProvider.transactions;
 
-          return Column(
-            children: [
-              Expanded(
-                child: transactions.isEmpty
-                    ? Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.receipt_long_outlined,
-                              size: 64,
-                              color: Theme.of(context).colorScheme.primary.withAlpha(128),
-                            ),
-                            const SizedBox(height: 16),
-                            Text(
-                              'No transactions yet',
-                              style: Theme.of(context).textTheme.titleLarge,
-                            ),
-                            const SizedBox(height: 8),
-                            Text(
-                              'Tap + to add your first transaction',
-                              style: Theme.of(context).textTheme.bodyMedium,
-                            ),
-                          ],
-                        ),
-                      )
-                    : ListView.builder(
-                        itemCount: transactions.length,
-                        padding: const EdgeInsets.all(8),
-                        itemBuilder: (context, index) {
-                          final transaction = transactions[index];
-                          final category = categoryProvider.categories
-                              .firstWhere((c) => c.id == transaction.categoryId);
-
-                          return Card(
-                            margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                            child: ListTile(
-                              leading: CircleAvatar(
-                                backgroundColor: transaction.type.name == 'income'
-                                    ? Colors.green
-                                    : Colors.red,
-                                child: Icon(
-                                  transaction.type.name == 'income'
-                                      ? Icons.arrow_downward
-                                      : Icons.arrow_upward,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              title: Text(getLocalizedCategoryName(context, category.name)),
-                              subtitle: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  if (transaction.description != null)
-                                    Text(transaction.description!),
-                                  Text(
-                                    DateFormat.yMMMd(Localizations.localeOf(context).toString()).format(transaction.date),
-                                    style: Theme.of(context).textTheme.bodySmall,
-                                  ),
-                                ],
-                              ),
-                              trailing: Text(
-                                '${transaction.amount.toStringAsFixed(2)} ${transaction.currency}',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: transaction.type.name == 'income'
-                                      ? Colors.green
-                                      : Colors.red,
-                                ),
-                              ),
-                              onTap: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                    builder: (_) => TransactionFormScreen(
-                                      transaction: transaction,
+                  return Column(
+                    children: [
+                      Expanded(
+                        child: transactions.isEmpty
+                            ? Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.receipt_long_outlined,
+                                      size: 64,
+                                      color: Theme.of(
+                                        context,
+                                      ).colorScheme.primary.withAlpha(128),
                                     ),
-                                  ),
-                                );
-                              },
-                            ),
-                          );
-                        },
+                                    const SizedBox(height: 16),
+                                    Text(
+                                      'No transactions yet',
+                                      style: Theme.of(
+                                        context,
+                                      ).textTheme.titleLarge,
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Text(
+                                      'Tap + to add your first transaction',
+                                      style: Theme.of(
+                                        context,
+                                      ).textTheme.bodyMedium,
+                                    ),
+                                  ],
+                                ),
+                              )
+                            : ListView.builder(
+                                itemCount: transactions.length,
+                                padding: const EdgeInsets.all(8),
+                                itemBuilder: (context, index) {
+                                  final transaction = transactions[index];
+                                  final category = categoryProvider.categories
+                                      .firstWhere(
+                                        (c) => c.id == transaction.categoryId,
+                                      );
+
+                                  final targetCurrency =
+                                      settingsProvider.targetCurrency;
+                                  final showConversion =
+                                      transaction.currency != targetCurrency;
+
+                                  return Card(
+                                    margin: const EdgeInsets.symmetric(
+                                      vertical: 4,
+                                      horizontal: 8,
+                                    ),
+                                    child: ListTile(
+                                      leading: CircleAvatar(
+                                        backgroundColor:
+                                            transaction.type.name == 'income'
+                                            ? Colors.green
+                                            : Colors.red,
+                                        child: Icon(
+                                          transaction.type.name == 'income'
+                                              ? Icons.arrow_downward
+                                              : Icons.arrow_upward,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      title: Text(
+                                        getLocalizedCategoryName(
+                                          context,
+                                          category.name,
+                                        ),
+                                      ),
+                                      subtitle: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          if (transaction.description != null)
+                                            Text(transaction.description!),
+                                          Text(
+                                            DateFormat.yMMMd(
+                                              Localizations.localeOf(
+                                                context,
+                                              ).toString(),
+                                            ).format(transaction.date),
+                                            style: Theme.of(
+                                              context,
+                                            ).textTheme.bodySmall,
+                                          ),
+                                        ],
+                                      ),
+                                      trailing: FutureBuilder<double>(
+                                        future: showConversion
+                                            ? currencyProvider.convert(
+                                                transaction.amount,
+                                                transaction.currency,
+                                                targetCurrency,
+                                              )
+                                            : Future.value(transaction.amount),
+                                        builder: (context, snapshot) {
+                                          final convertedAmount =
+                                              snapshot.data ??
+                                              transaction.amount;
+
+                                          return Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.end,
+                                            children: [
+                                              Text(
+                                                '${transaction.amount.toStringAsFixed(2)} ${transaction.currency}',
+                                                style: TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.bold,
+                                                  color:
+                                                      transaction.type.name ==
+                                                          'income'
+                                                      ? Colors.green
+                                                      : Colors.red,
+                                                ),
+                                              ),
+                                              if (showConversion &&
+                                                  snapshot.hasData)
+                                                Text(
+                                                  '≈ ${convertedAmount.toStringAsFixed(2)} $targetCurrency',
+                                                  style: TextStyle(
+                                                    fontSize: 12,
+                                                    color: Theme.of(context)
+                                                        .textTheme
+                                                        .bodySmall
+                                                        ?.color,
+                                                  ),
+                                                ),
+                                            ],
+                                          );
+                                        },
+                                      ),
+                                      onTap: () {
+                                        Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                            builder: (_) =>
+                                                TransactionFormScreen(
+                                                  transaction: transaction,
+                                                ),
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                  );
+                                },
+                              ),
                       ),
-              ),
-            ],
-          );
-        },
-      ),
+                    ],
+                  );
+                },
+          ),
     );
   }
 }
